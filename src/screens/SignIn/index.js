caa1fe8f9ff08135df73dd8f7a25538efdb262fc
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-09-08 23:05:58 -0300

    Primeiro commit

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from './node_modules/react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export default () => {
+    return (
+        <Container>
+            <Text>SigIn</Text>
+        </Container>
+    );
+}
---
+++ src/screens/SignIn/styles.js
@@ -0,0 +1,4 @@
+import React from './node_modules/react';
+import styled from './node_modules/styled-components/native';
+
+export const Container = styled.SafeAreaView``;
